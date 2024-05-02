ab14eefe36ae62a158726f07d25ec971e64beb47
phaxw <penunurisantiago20@gmail.com>
2024-05-01 18:50:54 -0700

    Se agregan problemas viejos que resolvi

---
+++ src/easy/ArrayPermutation.java
@@ -0,0 +1,13 @@
+package easy;
+
+public class ArrayPermutation {
+
+    public int[] buildArray(int[] nums) {
+        int [] res = new int[nums.length];
+        for (int i = 0; i<nums.length; i++){
+            res[i] = nums[nums[i]];
+        }
+        return res;
+    }
+
+}
---
+++ src/easy/SortedSquares.java
@@ -0,0 +1,16 @@
+package easy;
+
+import java.util.Arrays;
+
+public class SortedSquares {
+
+    public int[] sortedSquares(int[] nums) {
+        int [] res = new int[nums.length];
+
+        for (int i=0; i<nums.length; i++){
+            res[i] = nums[i]*nums[i];
+        }
+        Arrays.sort(res);
+        return res;
+    }
+}
